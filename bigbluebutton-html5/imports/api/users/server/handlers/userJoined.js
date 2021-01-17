d19d8f58a62748191b7230539c47e6c37cf2ded2
Ubuntu <ubuntu@ip-172-31-8-33.ap-south-1.compute.internal>
2021-01-17 16:02:38 +0000

    [feat] Adds attendance tracking to 2.2.x release

--- bigbluebutton-html5/imports/api/users/server/handlers/userJoined.js
+++ bigbluebutton-html5/imports/api/users/server/handlers/userJoined.js
@@ -1,11 +1,12 @@
 import { check } from 'meteor/check';
 
 import addUser from '../modifiers/addUser';
+import { attendanceOnJoin } from '../methods/submitAttendance';
 
 export default function handleUserJoined({ body }, meetingId) {
   const user = body;
 
   check(user, Object);
-
+  attendanceOnJoin(meetingId, user.extId);
   addUser(meetingId, user);
 }
